c7257140b136f056f1b313efa4e28c4ae6673e93
Humzaa Omar <70377687+hfomar-nafcs@users.noreply.github.com>
2021-01-26 15:15:14 -0500

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,25 @@
+/*	GOOGLE
+	----------------------------------------------------- */
+
+eng.google = {
+	pageTitle: "Google",
+	logo: "../images/google.png",
+	places: {
+		'Web'    : ["https://www.google.com/search?q=%query%", false],
+		'Images' : ["https://images.google.com/images?q=%query%", false],
+		'Maps'   : ["https://maps.google.com/maps?q=%query%", false]
+	}
+};
+
+/*	SubsPlease
+	----------------------------------------------------- */
+
+	eng.subsplease = {
+		pageTitle: "Subsplease",
+		logo: "../images/subsplease.png",
+		places: {
+			'Shows'    : ["https://cse.google.com/cse?cx=d41aff8dfcfbfd122&q=%query%", false]
+		}
+	};
+	
+
